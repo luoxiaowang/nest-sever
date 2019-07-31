c870cc145192a22342ac2449150aea70ba499247
luoxiao <yixing@xiaohongshu.com>
2019-07-31 23:25:22 +0800

    modify ip

--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
 export const port = process.env.PORT || 3000
-export const connexionString = 'mongodb://localhost/hivebox'
+// export const connexionString = 'mongodb://localhost/hivebox'
+export const connexionString = 'mongodb://176.122.171.171/db'
 export const baseApi = 'api'
